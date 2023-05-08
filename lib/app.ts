d03ada2a918aa103486bb8e3a5e97135fe644860
Erwin Villejo <erwinvillejo@gmail.com>
2023-05-08 12:37:51 +0800

    init s3 client

--- lib/app.ts
+++ lib/app.ts
@@ -1,13 +1,17 @@
-// import {S3Client} from '@aws-sdk/client-s3'
+import { S3Client } from '@aws-sdk/client-s3'
 import Router from '@koa/router'
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import { err } from './error.js'
 import { logger } from './logger.cjs'
 
+const s3Client = new S3Client({})
+
 export default function App() {
   const router = new Router()
-  router.get(/.*/, async (ctx) => {})
+  router.get(/.*/, async (ctx) => {
+    ctx.status = 200
+  })
 
   const app = new Koa()
   app
